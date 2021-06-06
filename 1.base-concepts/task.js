137fe16b900007fadc635e82dc6bf8ffb8927e3d
Anton Lyalkov <alyalkov@gmail.com>
2021-06-06 13:22:43 +0200

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
+"use strict"
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let D = Math.pow(b,2)-4*a*c;
+  if(d < 0) {
+    return [];
+  }
+  if (d === 0) {
+
+  } 
+  if (d > 0) {
+
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
