45396a473fe154841ffd85244322fb0aa49a2dd0
Anton Lyalkov <alyalkov@gmail.com>
2021-06-11 12:42:13 +0200

    task 2.2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,37 +1,37 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
-  min = 101;
-  max = -101;
+  min = Infinity;
+  max = -Infinity;
   sum = 0;
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] > max) {
     max = arr[i];
-  }
-  if (arr[i] < min) {
+  } else if (arr[i] < min) {
     min = arr[i];
   }
   sum+=arr[i];
-  avg = sum / arr[i];
-  avg.toFixed(2);
 }
+  avg = (sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++)
+  sum += arr[i];
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
+function mincer(arrOfArr, func) {
   let max
-
-  // Ваш кода
-  // for ...
+  for (let i=0; i<arrOfArr.length; i++) {
+    let sumOfArr = worker(arrOfArr[i]);
+    if (sumOfArr > max) {
+      max = sumOfArr;
+    }
+  }
   
   return max
 }
