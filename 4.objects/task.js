45f34f044eb37ea81143a8fde1f2b2d88686b21a
Anton Lyalkov <alyalkov@gmail.com>
2021-06-17 22:14:34 +0200

    tasks 4.1.2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,31 +1,37 @@
 function Student(name, gender, age) {
-   
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
-const Student1 = new Student;
-const Student2 = new Student;
-const Student3 = new Student;
 
 Student.prototype.setSubject = function (subjectName) {
- 
+  this.subject = subjectName;
 }
 
-Student.prototype.addMark = function (mark) {
+Student.prototype.addMark = function(mark) {
   if(this.marks === undefined){ 
+    this.marks = [];
     this.marks.push(mark);
     } else { 
     this.marks.push(mark);
     }
 }
 
-Student.prototype.addMarks = function (...mark) {
-
+Student.prototype.addMarks = function(...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
 }
 
-Student.prototype.getAverage = function () {
-
+Student.prototype.getAverage = function() {
+let sum = 0;
+for (let i = 0; i < this.marks.length; i++) {
+  sum+=this.marks[i];
+  return sum/this.marks[i];
+}
 }
 
-Student.prototype.exclude = function (reason) {
-
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
