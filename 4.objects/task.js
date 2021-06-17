f4346f59ee45c5068977180be1a66e25c3fc785e
Anton Lyalkov <alyalkov@gmail.com>
2021-06-17 21:47:29 +0200

    task 4.1.1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,31 @@
 function Student(name, gender, age) {
-    // Ваш код
+   
 
 }
+const Student1 = new Student;
+const Student2 = new Student;
+const Student3 = new Student;
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ 
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks.push(mark);
+    } else { 
+    this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+
+}
+
+Student.prototype.getAverage = function () {
+
+}
+
+Student.prototype.exclude = function (reason) {
+
+}
