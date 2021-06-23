03d18188e58b9b189fce5b3408b2dfac0fab89e9
Anton Lyalkov <alyalkov@gmail.com>
2021-06-23 23:05:21 +0200

    task 3 finish

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,11 @@
 function compareArrays(arr1, arr2) {
 
-  let result = (arr1.length === arr2.length) && arr1.every((item, idx) => item === arr2[idx]);
+  return Array.isArray(arr1) && Array.isArray(arr2) && (arr1.length === arr2.length) && arr1.every((item, idx) => item === arr2[idx]);
 
-  return result;
 }
 
 function advancedFilter(arr) {
 
-  let resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
+  return  arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
 
-  return resultArr;
 }
